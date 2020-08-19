438c45878a0c2b11fcbb6ffe440a75822425d342
Carmelo Ricci <carmeloricci@MacBook-Pro-di-Carmelo.local>
2020-08-19 11:31:23 +0200

    ok

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -78,33 +78,35 @@ const passport = require('passport');
 
 require('./passport').setupStrategies(passport);
 
-const pubApiDNSRoute = require('./routes/public/dnsRoutes');
-const pubApiUserRoutes = require('./routes/public/userRoutes')(passport);
+// const pubApiDNSRoute = require('./routes/public/dnsRoutes');
+// const pubApiUserRoutes = require('./routes/public/userRoutes')(passport);
 
-const prvApiTenantRoute = require('./routes/private/tenantRoutes');
-const prtApiUserRoutes = require('./routes/private/userRoutes');
-const prtApiDeviceRoutes = require('./routes/private/deviceRoutes');
+// const prvApiTenantRoute = require('./routes/private/tenantRoutes');
+// const prtApiUserRoutes = require('./routes/private/userRoutes');
+// const prtApiDeviceRoutes = require('./routes/private/deviceRoutes');
 
-app.use('/dns', pubApiDNSRoute);
-app.use('/user', pubApiUserRoutes);
+// app.use('/dns', pubApiDNSRoute);
+// app.use('/user', pubApiUserRoutes);
 
-app.use('/api/private/device', authenticate, prtApiDeviceRoutes);
-app.use('/api/private/tenant', authenticate, prvApiTenantRoute);
-app.use('/api/private/user', authenticate, prtApiUserRoutes);
+// app.use('/api/private/device', authenticate, prtApiDeviceRoutes);
+// app.use('/api/private/tenant', authenticate, prvApiTenantRoute);
+// app.use('/api/private/user', authenticate, prtApiUserRoutes);
 
 
 
-const frontendPublic = path.join(__dirname, '../../frontend/public');
-const frontendDist = path.join(__dirname, '../../frontend/dist');
-app.use('/public', express.static(frontendPublic));
-app.use('/dist', express.static(frontendDist));
-app.get('*', (req, res, next) => {
-  res.sendFile(path.join(frontendPublic, '/index.html'));
-});
+// const frontendPublic = path.join(__dirname, '../../frontend/public');
+// const frontendDist = path.join(__dirname, '../../frontend/dist');
+// app.use('/public', express.static(frontendPublic));
+// app.use('/dist', express.static(frontendDist));
+// app.get('*', (req, res, next) => {
+//   res.sendFile(path.join(frontendPublic, '/index.html'));
+// });
 
 // Avvia il job che sta in ascolto in modo da intercettare i cambiamenti sul file dnsmasq.leases 
 // ad ogni modifica verrà richiamato il metodo per scansionare l'arp table e contattare i GW
-require('./shared/watcher');
+// require('./shared/watcher');
+
+
 app.listen(3880, () => {
   console.log('Application listening on port 3880!');
 });
